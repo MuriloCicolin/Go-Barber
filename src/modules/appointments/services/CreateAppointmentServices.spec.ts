6a71b026048ae428b3b47ebd657b1f9f4d433ae3
MuriloCicolin <mu_cicolin@hotmail.com>
2020-05-12 13:56:42 -0300

    Criando primeiro teste

--- src/modules/appointments/services/CreateAppointmentServices.spec.ts
+++ src/modules/appointments/services/CreateAppointmentServices.spec.ts
@@ -1,3 +1,19 @@
-test('sum two numbers', () => {
-  expect(1 + 2).toBe(3);
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentRepository';
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointment = await createAppointment.execute({
+      provider_id: '123123',
+      date: new Date(),
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('123123');
+  });
 });
