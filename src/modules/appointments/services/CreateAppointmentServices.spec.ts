ad9f6ba43c7b012b72ad613b340e16913f77e1df
MuriloCicolin <mu_cicolin@hotmail.com>
2020-05-12 14:39:45 -0300

    Testes de agendamentos

--- src/modules/appointments/services/CreateAppointmentServices.spec.ts
+++ src/modules/appointments/services/CreateAppointmentServices.spec.ts
@@ -1,3 +1,4 @@
+import AppError from '@shared/errors/AppError';
 import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentRepository';
 import CreateAppointmentService from './CreateAppointmentService';
 
@@ -15,5 +16,26 @@ describe('CreateAppointment', () => {
 
     expect(appointment).toHaveProperty('id');
     expect(appointment.provider_id).toBe('123123');
+  });
+
+  it('should not be able to create two appointments in the same time', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointmentDate = new Date(2020, 4, 12, 11);
+
+    await createAppointment.execute({
+      provider_id: '123123',
+      date: appointmentDate,
+    });
+
+    expect(
+      createAppointment.execute({
+        provider_id: '123123',
+        date: appointmentDate,
+      }),
+    ).rejects.toBeInstanceOf(AppError);
   });
 });
