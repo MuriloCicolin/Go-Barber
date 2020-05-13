ce67081612a99265030377eddaafce38eba9a01b
MuriloCicolin <mu_cicolin@hotmail.com>
2020-05-13 14:20:46 -0300

    Teste SendForgoPasswordEmailService

---
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -0,0 +1,25 @@
+import { injectable, inject } from 'tsyringe';
+
+import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
+import IUsersRepository from '../repositories/IUsersRepository';
+
+interface IRequest {
+  email: string;
+}
+
+@injectable()
+class SendForgotEmailPasswordService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+
+    @inject('MailProvider')
+    private mailProvider: IMailProvider,
+  ) {}
+
+  public async execute({ email }: IRequest): Promise<void> {
+    this.mailProvider.sendMail(email, 'Pedido de recuperação recebido');
+  }
+}
+
+export default SendForgotEmailPasswordService;
