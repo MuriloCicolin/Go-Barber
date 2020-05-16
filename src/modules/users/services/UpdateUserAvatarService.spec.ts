5172c065d9ec6c2b70ca20f88c1c961b7f6745df
MuriloCicolin <mu_cicolin@hotmail.com>
2020-05-16 13:23:53 -0300

    Refatoracao de testes

--- src/modules/users/services/UpdateUserAvatarService.spec.ts
+++ src/modules/users/services/UpdateUserAvatarService.spec.ts
@@ -3,11 +3,16 @@ import AppError from '@shared/errors/AppError';
 import UpdateAvatarService from './UpdateUserAvatarService';
 import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
 
-describe('UpdateUserAvatar', () => {
-  it('should be able to update avatar', async () => {
-    const fakeUsersRepository = new FakeUsersRepository();
-    const fakeStorageProvider = new FakeStorageProvider();
+let fakeUsersRepository: FakeUsersRepository;
+let fakeStorageProvider: FakeStorageProvider;
 
+describe('UpdateUserAvatar', () => {
+  beforeEach(() => {
+    fakeUsersRepository = new FakeUsersRepository();
+    fakeStorageProvider = new FakeStorageProvider();
+  });
+
+  it('should be able to update avatar', async () => {
     const updateAvatar = new UpdateAvatarService(
       fakeUsersRepository,
       fakeStorageProvider,
@@ -28,9 +33,6 @@ describe('UpdateUserAvatar', () => {
   });
 
   it('should not be able to update avatar from non-existing-user', async () => {
-    const fakeUsersRepository = new FakeUsersRepository();
-    const fakeStorageProvider = new FakeStorageProvider();
-
     const updateAvatar = new UpdateAvatarService(
       fakeUsersRepository,
       fakeStorageProvider,
@@ -45,9 +47,6 @@ describe('UpdateUserAvatar', () => {
   });
 
   it('should delete old avatar when updating new one', async () => {
-    const fakeUsersRepository = new FakeUsersRepository();
-    const fakeStorageProvider = new FakeStorageProvider();
-
     const deleteFile = jest.spyOn(fakeStorageProvider, 'deleteFile');
 
     const updateAvatar = new UpdateAvatarService(
