7aeea4b35a8309ef3b0e79a7bbdd8672a35b29fd
MuriloCicolin <mu_cicolin@hotmail.com>
2020-05-19 19:28:32 -0300

    Excluindo horarios antigos

--- src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
+++ src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
@@ -15,32 +15,34 @@ describe('List Providers', () => {
   it('should be able to list the day availability from provider', async () => {
     await fakeAppointmentsRepository.create({
       provider_id: 'user',
-      date: new Date(2020, 4, 21, 8, 0, 0),
+      date: new Date(2020, 4, 20, 14, 0, 0),
     });
 
     await fakeAppointmentsRepository.create({
       provider_id: 'user',
-      date: new Date(2020, 4, 21, 10, 0, 0),
+      date: new Date(2020, 4, 20, 15, 0, 0),
     });
 
-    await fakeAppointmentsRepository.create({
-      provider_id: 'user',
-      date: new Date(2020, 4, 21, 11, 0, 0),
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2020, 4, 20, 11).getTime();
     });
 
     const availability = await listProviderDayAvailability.execute({
       provider_id: 'user',
       year: 2020,
       month: 5,
-      day: 21,
+      day: 20,
     });
 
     expect(availability).toEqual(
       expect.arrayContaining([
         { hour: 8, available: false },
-        { hour: 9, available: true },
+        { hour: 9, available: false },
         { hour: 10, available: false },
-        { hour: 11, available: false },
+        { hour: 13, available: true },
+        { hour: 14, available: false },
+        { hour: 15, available: false },
+        { hour: 16, available: true },
       ]),
     );
   });
--- src/modules/appointments/services/ListProviderDayAvailabilityService.ts
+++ src/modules/appointments/services/ListProviderDayAvailabilityService.ts
@@ -1,5 +1,5 @@
 import { inject, injectable } from 'tsyringe';
-import { getHours, getDate } from 'date-fns';
+import { getHours, isAfter } from 'date-fns';
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 
 interface IRequest {
@@ -43,14 +43,18 @@ class ListProviderMonthAvailabilityService {
       (value, index) => index + hourStart,
     );
 
+    const currentDate = new Date(Date.now());
+
     const availability = eachHourStart.map(hour => {
       const hasAppointmentInHour = appointments.find(
         appointment => getHours(appointment.date) === hour,
       );
 
+      const compareDate = new Date(year, month - 1, day, hour);
+
       return {
         hour,
-        available: !hasAppointmentInHour,
+        available: !hasAppointmentInHour && isAfter(compareDate, currentDate),
       };
     });
 
