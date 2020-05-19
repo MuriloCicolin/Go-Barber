f50d1d2ee4ac054e5299d636210712286b96856c
MuriloCicolin <mu_cicolin@hotmail.com>
2020-05-19 12:58:36 -0300

    Listando dias disponiveis

--- src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
+++ src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -1,5 +1,5 @@
 import { inject, injectable } from 'tsyringe';
-
+import { getDaysInMonth, getDate } from 'date-fns';
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 
 interface IRequest {
@@ -33,9 +33,24 @@ class ListProviderMonthAvailabilityService {
       },
     );
 
-    console.log(appointments);
+    const numberOfDaysInMonth = getDaysInMonth(new Date(year, month - 1));
 
-    return [{ day: 1, available: false }];
+    const eachDayArray = Array.from(
+      { length: numberOfDaysInMonth },
+      (value, index) => index + 1,
+    );
+
+    const availability = eachDayArray.map(day => {
+      const appointmentsInDay = appointments.filter(appointment => {
+        return getDate(appointment.date) === day;
+      });
+      return {
+        day,
+        available: appointmentsInDay.length < 10,
+      };
+    });
+
+    return availability;
   }
 }
 
