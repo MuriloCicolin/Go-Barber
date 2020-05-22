62c9852f36602d30d96ea65aaabade7d0c505d2e
MuriloCicolin <mu_cicolin@hotmail.com>
2020-05-21 22:04:49 -0300

    Cache lista de providers

--- src/modules/appointments/services/ListProvidersService.ts
+++ src/modules/appointments/services/ListProvidersService.ts
@@ -2,6 +2,7 @@ import { inject, injectable } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 
 import User from '@modules/users/infra/typeorm/entities/User';
 
@@ -14,15 +15,24 @@ class ListProvidersService {
   constructor(
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
+
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider,
   ) {}
 
   public async execute({ user_id }: IRequest): Promise<User[]> {
-    const user = await this.usersRepository.findAllProviders({
-      except_user_id: user_id,
-    });
+    let user = await this.cacheProvider.recover<User[]>(
+      `providers-list:${user_id}`,
+    );
 
     if (!user) {
-      throw new AppError('User not found');
+      user = await this.usersRepository.findAllProviders({
+        except_user_id: user_id,
+      });
+
+      console.log('A query no banco deu certo');
+
+      await this.cacheProvider.save(`providers-list:${user_id}`, user);
     }
 
     return user;
