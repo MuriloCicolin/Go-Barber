d3f0bf94452f82dc44a13d6338baf0ae6b4d667f
MuriloCicolin <mu_cicolin@hotmail.com>
2020-05-26 23:05:35 -0300

    Serializacao no cache

--- src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
+++ src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
+import { classToClass } from 'class-transformer';
 
 import ListProvidersAppointmentsService from '@modules/appointments/services/ListProvidersAppointmentsService';
 
@@ -19,6 +20,6 @@ export default class ProviderAppointmentsController {
       provider_id,
     });
 
-    return response.json(appointments);
+    return response.json(classToClass(appointments));
   }
 }
